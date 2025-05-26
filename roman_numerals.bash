740ba01210cd076f12d010c61917656f34f306b7
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2025-05-26 10:02:59 -0700

    start of fleshing out greedy algorithm

--- roman_numerals.bash
+++ roman_numerals.bash
@@ -1,26 +1,166 @@
 #! /bin/bash
 
-read num
-num_digits=${#num}
-c=$num_digits
-value=0
+SYNTAX=MODERN
+# EARLY, MODERN, VINCULUM, APOSTROPHUS
 
-for(c=$num_digits; c>=0; c--) ; do
-  if (num == 0) break;
-  digit = ${num:0:1}
-  print_digit($digit)
-done
+FORMAT=CLASSIC
+# Akin to conversion algorithm
+# 0. CLASSIC (digit by digit)
+# Order of Letters:  MDCLXVI
+#   1. largest closest L
+#   2. largest closest X
+#   3. largest closest V
+#   4. largest closest I
 
+
+MAX_EARLY_ROMAN_NUMBER=899
+MAX_MODERN_ROMAN_NUMBER=3999
+MAX_VINCULUM_ROMAN_NUMBER=999999999
+MAX_APOSTROPHUS_ROMAN_NUMBER=0
 
 
 declare -a roman_array=(
-  "I--"  "XVI"  "CLX"   "MDX"
+  "I--"  "XVI"  "CLX"  "MDC" "--M"
 )
 
-function roman_digit() {
+
+#155,3999
+#
+#(((I)))I)))I))
+
+
+# arabic2roman_classic
+function arabic2roman_xxx() {
+  number="$1"
+
+  count=${#number}
+  for ((c=0, d=${count}; c < ${count}; c++, d--)) ; do
+    digit=${number:$c:1}
+    digit2roman $digit ${roman_array[d]}
+  done
+}
+
+function arabic2roman(){
+  # Classical Form
+
+  number="$1"
+  
+  if ((number > ${MAX_ROMAN_NUMBER})) ; then
+    { echo "Current Syntax: ${SYNTAX}" ;
+      echo "Maximum Number: ${MAX_ROMAN_NUMBER}" ;
+    } > /dev/stderr
+    return 2
+  fi
+  if ((number == 0)) ; then
+    echo nila
+    return 1
+  fi
+
+  if [[ ${SYNTAX} == EARLY ]] ||
+     [[ ${SYNTAX} == MODERN ]] ; then
+    arabic2roman_xxx ${number}
+  fi
+
+  if [[ ${SYNTAX} == VINCULUM ]] ; then 
+    (( group3 = number / 1000 / 1000 ))
+    (( group2 = number / 1000 % 1000 ))
+    (( group1 = number % 1000 ))
+
+    if (( group3 > 0 )) ; then
+      echo -n "<vinculum>|"
+      arabic2roman_xxx $group3
+      echo -n "|<\vinculum>"
+    fi 
+    if (( group2 > 0 )) ; then
+      echo -n "<vinculum>"
+      arabic2roman_xxx $group2
+      echo -n "<\vinculum>"
+    fi  
+    arabic2roman_xxx $group1
+  fi
+    echo
+  return 0
+}
+
+# CLASSIC,
+function set_format () {
+  FORMAT="$1"
+
+  [[ -z ${FORMAT} ]] && SYNTAX=CLASSIC
+
+}
+
+# EARLY, MODERN, VINCULUM, APOSTROPHUS
+function set_syntax() {
+  SYNTAX="$1"
+  
+  [[ -z ${SYNTAX} ]] && SYNTAX=MODERN 
+
+  case ${SYNTAX} in
+    EARLY )
+      MAX_ROMAN_NUMBER=${MAX_EARLY_ROMAN_NUMBER}
+      ;;
+
+    MODERN )
+      MAX_ROMAN_NUMBER=${MAX_MODERN_ROMAN_NUMBER}
+      ;;
+
+    VINCULUM )
+      set_format CLASSIC
+      MAX_ROMAN_NUMBER=${MAX_VINCULUM_ROMAN_NUMBER}
+      ;;
+
+    APOSTROPHUS )
+      MAX_ROMAN_NUMBER=${MAX_VINCULUM_ROMAN_NUMBER}
+      ;;
+  esac
+}
+
+function set_defaults() {
+  set_half_form TRUE
+  set_subtractive_form TRUE
+  set_syntax MODERN
+  set_format 
+}
+
+function set_subtractive_form() {
+  _value="$1"
+
+  subtractive_form=TRUE      
+  subtractive_form_4=TRUE   # IV -versus- IIII
+  subtractive_form_8=FALSE  # VIII -versus-  IIX
+  subtractive_form_9=TRUE   # IX -versus- VIIII 
+  if [[ $_value == FALSE ]] ; then
+    subtractive_form=FALSE      
+    subtractive_form_4=FALSE   # IV -versus- IIII
+    subtractive_form_8=FALSE   # VIII -versus-  IIX
+    subtractive_form_9=FALSE   # IX -versus- VIIII
+  fi 
+}
+
+function set_half_form() {
+  _value="$1"
+
+  half_form=TRUE
+  if [[ $_value == FALSE ]] ; then
+    half_form=FALSE
+    subtractive_form_4=FALSE
+  fi
+}
+
+# Note it is an error/inconsistency if
+#   half_form == FALSE and subtractive_form_4 == TRUE
+#
+# Hence, subtractive_form_4 is ignored in half_form
+
+
+set_defaults
+
+
+function digit2roman() {
   digit=$1
-  array=$2
-  if [[ -z ${array} ]] ; then
+  place=$2
+  if [[ -z ${place} ]] ; then
     place=${roman_array[1]}
   fi
 
@@ -28,37 +168,57 @@ function roman_digit() {
   half=${place:1:1}
   base=${place:2:1}
 
+  if [[ ${half_form} == FALSE ]] ; then
+    half="${base}${base}${base}${base}${base}"
+  fi
+
   case ${digit} in
     1|2|3 )
-      for((i = digit; i>0; i--)) ; do
-        echo -n ${base}
+      for ((i = digit; i>0; i--)) ; do
+        echo -n "${base}"
       done
       ;;
 
     4 )
-      echo -n "${base}${half}"
+      echo -n "${base}"
+      if [[ ${subtractive_form_4} == TRUE ]] ; then 
+        echo -n "${half}"
+      else
+        echo -n "${base}${base}${base}"
+      fi
       ;;
 
-    5 | 6 | 7 )
-        echo -n "${half}"
-        for((i= digit - 5 ; i>0; i--)) ; do 
-          echo -n "${base}"
-        done
-        ;;
+    5 )
+      echo -n "${half}"
+      ;;
+
+    6 | 7 )
+      echo -n "${half}${base}"
+      if (( digit == 7 )) ; then
+        echo -n "${base}"
+      fi
+      ;;
+
     8 )
-        # if !  8-subtraction
-            echo -n "${half}${base}${base}${base}"
-        # else
-        #   echo -n "${base}${base}${unit}" 
+      if [[ ${subtractive_form_8} == FALSE ]] ; then
+        echo -n "${half}${base}${base}${base}"
+      else
+        echo -n "${base}${base}${unit}" 
+      fi
         ;; 
 
     9 )
-        echo -n "${base}"
+      echo -n "${base}"
+      if [[ ${subtractive_form_9} == TRUE ]] ; then 
         echo -n "${unit}"
-        ;;
+      else
+        echo -n "${base}${base}${base}"
+      fi
+      ;;
   esac
 
 }
+
 
 
 
